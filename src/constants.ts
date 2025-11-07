1f8852ef6b8d4d2fa2daad8e81bb49c712ba4154
Thatcher Eames <thatcher.eames@comcast.net>
2025-11-07 11:20:45 -0800

    Added a default font to the constants file

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,10 @@ export const STAGE_HEIGHT = 600;
 //Standard game colors
 export const BCKGRD_COLOR = "#FFFFCD";
 export const HIGHLIGHT_COLOR = "#FD754D";
-export const ALERT_COLOR = "#F94449"
+export const ALERT_COLOR = "#F94449";
+
+//Standard font
+export const DEFAULT_FONT = "Impact";
 
 // Game settings
 export const GAME_DURATION = 10; // seconds
@@ -16,5 +19,6 @@ export const GAMECST = {
     BCKGRD_COLOR,
     HIGHLIGHT_COLOR,
     ALERT_COLOR,
+    DEFAULT_FONT,
     GAME_DURATION
 } as const;
