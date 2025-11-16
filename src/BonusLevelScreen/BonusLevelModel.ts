7bb21f8326438b12e44b25b7a90e6b1653f973dd
EdtheEgghead <edzeegger@gmail.com>
2025-11-16 14:34:05 -0800

    Adds BonusLevelModel to store division operands and player inputs

--- src/BonusLevelScreen/BonusLevelModel.ts
+++ src/BonusLevelScreen/BonusLevelModel.ts
@@ -1,9 +1,18 @@
 /**
- * BonusLevelModel - Stores game state
+ * BonusLevelModel - Stores game state for the bonus round
  */
 export class BonusLevelModel {
+  dividend: number;
+  divisor: number;
+  quotient: number;
+  playerInput: string;
+  resultMessage: string;
 
-    constructor() {
-
-    }
+  constructor() {
+    this.dividend = 0;
+    this.divisor = 0;
+    this.quotient = 0;
+    this.playerInput = "";
+    this.resultMessage = "";
+  }
 }
