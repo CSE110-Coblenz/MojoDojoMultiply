fb11dd19d1f919128b7816110d6744b088dfa84d
EdtheEgghead <edzeegger@gmail.com>
2025-11-17 12:24:38 -0800

    Adds bonusLevel controllers to main and adds a switcher to bonus from roundStats

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { PracticeAreaController } from "./PracticeAreaScreen/PracticeAreaControl
 import { ResultsScreenController} from "./ResultsPageScreen/ResultsPageController";
 import { RoundIntroController } from "./RoundIntroScreen/RoundIntroController";
 import { RoundStatsController } from "./RoundStatsScreen/RoundStatsController";
+import { BonusLevelController } from "./BonusLevelScreen/BonusLevelController";
 
 class App implements ScreenSwitcher {
   private stage: Konva.Stage;
@@ -21,6 +22,7 @@ class App implements ScreenSwitcher {
   private resultsController: ResultsScreenController;
   private roundIntroController: RoundIntroController;
   private roundStatsController: RoundStatsController;
+  private bonusLevelController: bonusLevelController;
 
   constructor(containerId: string) {
     this.stage = new Konva.Stage({
@@ -39,6 +41,7 @@ class App implements ScreenSwitcher {
     this.resultsController = new ResultsScreenController(this);
     this.roundIntroController = new RoundIntroController(this);
     this.roundStatsController = new RoundStatsController(this);
+    this.bonusLevelController = new BonusLevelController(this);
 
     // Add screen groups to same layer
     this.layer.add(this.startController.getView().getGroup());
@@ -48,6 +51,7 @@ class App implements ScreenSwitcher {
     this.layer.add(this.resultsController.getView().getGroup());
     this.layer.add(this.roundIntroController.getView().getGroup());
     this.layer.add(this.roundStatsController.getView().getGroup());
+    this.layer.add(this.bonusLevelController.getView().getGroup());
 
     this.layer.draw();
 
@@ -64,6 +68,7 @@ class App implements ScreenSwitcher {
     this.resultsController.hide();
     this.roundIntroController.hide();
     this.roundStatsController.hide();
+    this.bonusLevelController.hide();
 
     switch (screen.type) {
       case "start": this.startController.show(); break;
@@ -79,7 +84,10 @@ class App implements ScreenSwitcher {
         this.roundStatsController.show();
         break;
       case "results":
-        this.resultsController.show()
+        this.resultsController.show();
+        break;
+      case "bonus":
+        this.bonusController.show();
         break;
     }
   }
