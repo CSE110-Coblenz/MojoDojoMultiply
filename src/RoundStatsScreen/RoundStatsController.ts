1d2e683a5904020eeacd0f9922d3855f1a0280f9
EdtheEgghead <edzeegger@gmail.com>
2025-11-17 16:38:34 -0800

    Reverts RoundStatsController back to original

--- src/RoundStatsScreen/RoundStatsController.ts
+++ src/RoundStatsScreen/RoundStatsController.ts
@@ -1,6 +1,5 @@
 import { ScreenController, type ScreenSwitcher } from "../types";
 import { RoundStatsView } from "./RoundStatsView";
-import { GAMECST } from "../constants";
 
 export class RoundStatsController extends ScreenController {
   private screenSwitcher: ScreenSwitcher;
@@ -21,15 +20,10 @@ export class RoundStatsController extends ScreenController {
   }
 
   private nextRound(): void {
-    if (this.round > 0 && this.round % GAMECST.ROUNDS_UNTIL_BONUS === 0) {
-      // Time for the bonus round
-      this.screenSwitcher.switchToScreen({ type: "bonus" });
-    } else {
-      // Go to the next regular round
-      this.screenSwitcher.switchToScreen({
-        type: "intro",
-        round: this.round + 1,
-      });
+    this.screenSwitcher.switchToScreen({
+      type: "intro",
+      round: this.round + 1,
+    });
   }
 
   show(): void { this.view.show(); }
