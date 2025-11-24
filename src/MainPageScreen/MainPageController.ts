b60f44b53e118bdf38ba0dfbb94229befcf50e0c
EdtheEgghead <edzeegger@gmail.com>
2025-11-24 12:35:07 -0800

    Makes MainPageController switch to bonus round on game end

--- src/MainPageScreen/MainPageController.ts
+++ src/MainPageScreen/MainPageController.ts
@@ -724,13 +724,8 @@ export class MainPageController extends ScreenController {
                 this.updateScore(400);
             }
 
-            // TODO need to decide if we want to go maingame --> bonus --> stats or maingame --> stats --> bonus etc.
             // Check if we should go to the bonus level
-            if (this.model.currentRound % GAMECST.ROUNDS_UNTIL_BONUS === 0) {
-                this.screenSwitcher.switchToScreen({ type: "bonus" });
-            } else {
-                this.screenSwitcher.switchToScreen({ type: "stats"});
-            }
+            this.screenSwitcher.switchToScreen({ type: "stats"});
         }
     }
 
